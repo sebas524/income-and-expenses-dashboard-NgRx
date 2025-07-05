31b6f0c51f4cb01670edde96735003409158460e
sebas524 <velezjsebastian@gmail.com>
2025-07-05 15:19:40 -0500

    add username details from state in menu and nav bar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { AppState } from '../../../app.reducer';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [TitleCasePipe],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  private store = inject(Store<AppState>);
+  userSubs!: Subscription;
+  username: string | undefined = '';
 
+  ngOnInit(): void {
+    this.userSubs = this.store.select('user').subscribe((state) => {
+      if (state.user) {
+        this.username = state.user.name;
+      } else {
+        this.username = undefined;
+      }
+    });
+  }
 }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,16 +1,39 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../../auth/services/auth.service';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../app.reducer';
+import { Subscription } from 'rxjs';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, TitleCasePipe],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit, OnDestroy {
   authService = inject(AuthService);
   router = inject(Router);
+  private store = inject(Store<AppState>);
+  userSubs!: Subscription;
+  username: string | undefined = '';
+  email: string | undefined = '';
+  ngOnInit(): void {
+    this.userSubs = this.store.select('user').subscribe((state) => {
+      if (state.user) {
+        this.username = state.user.name;
+        this.email = state.user.email;
+      } else {
+        this.username = undefined;
+        this.email = undefined;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSubs.unsubscribe();
+  }
   logout() {
     this.authService.logout().then(() => {
       this.router.navigate(['/login']);
